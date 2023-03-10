7a783fb1d3c9405ee3fbc6287a3222167b72c632
Harshith <harshithgv.sangamone@gmail.com>
2023-03-10 17:47:38 +0530

    Agora UIKIT

---
+++ src/Components/Login.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsValid(username && password);
+  }, [username, password]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Form submitted");
+    console.log("username: ", username);
+    console.log("password: ", password);
+    try {
+      const response = await axios.post(
+        "http://navatarbe-env.eba-drviydr6.us-east-2.elasticbeanstalk.com/NavatarLogin",
+        {
+          username: username,
+          password: password,
+        }
+      );
+      console.log("response: ", response.data);
+      if (response.status === 200 && response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/Listofbooking");
+      } else {
+        navigate("/Listofbooking");
+      }
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("Invalid username or password.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+      {errorMessage && <div>{errorMessage}</div>}
+      <div>
+        <h1>Navatar Login</h1>
+ 
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          placeholder="Username"
+          required
+        />
+      </div>
+      <br></br>
+      <div>
+        
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder="password"
+          required
+        />
+      </div>
+      <br></br>
+     <button to="/Home" type="submit" disabled={!isValid}>
+        Login
+      </button>
+   
+    </form>
+  );
+}
+
+export default LoginForm;
